c5d112c409f945ac232f1cc9760e45d97a21f736
joffinjoy <joffinjoy@gmail.com>
2024-03-20 18:44:47 +0530

    Made Limiter Windows & Limits Dynamic

--- src/app.js
+++ src/app.js
@@ -12,14 +12,13 @@ if (!environmentData.success) {
 }
 require('./init')
 
-/* packageInstaller(process.env.REQUIRED_PACKAGES).catch((error) => {
+packageInstaller(process.env.REQUIRED_PACKAGES).catch((error) => {
 	console.error(`An error occurred in package installer: ${error}`)
 	process.exit()
-}) */
+})
 
 const app = express()
 const path = require('path')
-//const packageValidator = require('./utils/packageValidator');
 
 //Package Loader & Validation
 const routerPackages = require('@utils/packageLoader').packageLoader()
@@ -27,17 +26,12 @@ const routerPackages = require('@utils/packageLoader').packageLoader()
 const validatedPackages = routerPackages //Bypassing the validator for now
 
 app.use(cors())
-/* app.use(bodyParser.urlencoded({ extended: true, limit: '50MB' }));
-app.use(bodyParser.json({ limit: '50MB' })); */
 
-//Router
 const { initializeRouter } = require('@router')
 app.use(initializeRouter(validatedPackages))
 app.get(process.env.API_DOC_URL, function (req, res) {
 	res.sendFile(path.join(__dirname, './api-doc/index.html'))
 })
-/* const { initializeOrchestrationRouter } = require('./router/orchestrationRouter');
-app.use('/interface', initializeOrchestrationRouter()); */
 
 app.listen(process.env.APPLICATION_PORT, (res, err) => {
 	if (err) {
--- src/constants/rateLimitConfigs.js
+++ src/constants/rateLimitConfigs.js
@@ -9,22 +9,22 @@ const standardHandler = (req, res) => {
 
 exports.rateLimitConfigs = {
 	'public-low': {
-		windowMs: 2 * 60 * 1000,
-		limit: 5,
+		windowMs: process.env.RATE_LIMITER_PUBLIC_LOW_WINDOW,
+		limit: process.env.RATE_LIMITER_PUBLIC_LOW_LIMIT,
 		standardHeaders: false,
 		legacyHeaders: false,
 		handler: standardHandler,
 	},
 	general: {
-		windowMs: 1 * 60 * 1000,
-		limit: 50,
+		windowMs: process.env.RATE_LIMITER_GENERAL_WINDOW,
+		limit: process.env.RATE_LIMITER_GENERAL_LIMIT,
 		standardHeaders: false,
 		legacyHeaders: false,
 		handler: standardHandler,
 	},
 	internal: {
-		windowMs: 1 * 60 * 1000,
-		limit: 50,
+		windowMs: process.env.RATE_LIMITER_INTERNAL_WINDOW,
+		limit: process.env.RATE_LIMITER_INTERNAL_LIMIT,
 		standardHeaders: false,
 		legacyHeaders: false,
 		handler: standardHandler,
