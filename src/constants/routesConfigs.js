ad0a182d8c7127bf79433627c675f59aaa1a5674
adithya_dinesh <adithya@shikshalokam.org>
2024-04-23 14:38:17 +0530

    Interface service added self creation portal

--- src/constants/routesConfigs.js
+++ src/constants/routesConfigs.js
@@ -2,6 +2,6 @@
 const config = require('@constants/config.json')
 
 exports.routesConfigs = {
-	services: ['user', 'mentoring', 'notification', 'scheduler'],
+	services: ['user', 'mentoring', 'notification', 'scheduler', 'self_creation_portal'],
 	routes: config.routes,
 }
