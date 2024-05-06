6194f558fb4634c9e4a215a6c5e43fc945ed32e4
vishnu <vishnu@tunerlabs.com>
2024-05-06 16:20:16 +0530

    package name changed to entity_management

--- src/constants/routesConfigs.js
+++ src/constants/routesConfigs.js
@@ -2,6 +2,6 @@
 const config = require('@constants/config.json')
 
 exports.routesConfigs = {
-	services: ['user', 'mentoring', 'notification', 'scheduler', 'project', 'entity-management','self_creation_portal'],
+	services: ['user', 'mentoring', 'notification', 'scheduler', 'project', 'entity_management','self_creation_portal'],
 	routes: config.routes,
 }
