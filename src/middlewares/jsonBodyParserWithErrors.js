0a83a4aa4a0a85ca8ec67edb26dbaa5d2193fbb3
joffinjoy <joffinjoy@gmail.com>
2024-07-12 11:39:34 +0530

    Added Dependency Config

--- src/middlewares/jsonBodyParserWithErrors.js
+++ src/middlewares/jsonBodyParserWithErrors.js
@@ -2,14 +2,14 @@
 const bodyParser = require('body-parser')
 
 exports.jsonBodyParserWithErrors = (req, res, next) => {
-    bodyParser.json({ limit: '50MB' })(req, res, err => {
-        if (err instanceof SyntaxError || (err && err.type === 'entity.parse.failed')) {
-            res.status(400).json({
-                responseCode: 'INVALID_JSON', // response code and message can be customized here
-                message: 'The JSON provided in the request is invalid or malformed.',
-            });
-        } else {
-            next(err);
-        }
-    });
-};
+	bodyParser.json({ limit: '50MB' })(req, res, (err) => {
+		if (err instanceof SyntaxError || (err && err.type === 'entity.parse.failed')) {
+			res.status(400).json({
+				responseCode: 'INVALID_JSON', // response code and message can be customized here
+				message: 'The JSON provided in the request is invalid or malformed.',
+			})
+		} else {
+			next(err)
+		}
+	})
+}
