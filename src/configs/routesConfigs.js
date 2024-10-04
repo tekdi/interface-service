ef2338a8b6590837bcb2a09aebf4c03fd37b74fb
adithya_dinesh <adithya@shikshalokam.org>
2024-10-04 10:53:59 +0530

    base package name change

--- src/configs/routesConfigs.js
+++ src/configs/routesConfigs.js
@@ -2,6 +2,6 @@
 const config = require('@configs/.config.json')
 
 exports.routesConfigs = {
-	services: ['user', 'mentoring', 'notification', 'scheduler', 'project', 'entity_management','self_creation_portal','survey'],
+	services: ['user', 'mentoring', 'notification', 'scheduler', 'project', 'entity-management','self-creation-portal','survey'],
 	routes: config.routes,
 }
